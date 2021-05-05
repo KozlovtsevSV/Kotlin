5978a0e6b48800c2e62a81bec5824ea567479abf
Sergey <KozlovtsevSV@gmail.com>
2021-05-05 18:04:38 +0300

    d

--- lesson01/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ lesson01/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,39 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonClick = findViewById<Button>(R.id.click)
-        val buttonView = findViewById<Button>(R.id.view)
-
-        var listMyDataClass: ArrayList<MyDataClass> = ArrayList()
-
-        if (buttonClick != null) {
-            buttonClick.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    for (i in 1..10) {
-                        listMyDataClass.add(createMyDataClass("MyDataClass_" + i, i))
-                    }
-                }
-            })
-        }
-
-
-        if (buttonView != null) {
-            buttonView.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    for (i in 0..listMyDataClass.size-1) {
-                        println(listMyDataClass.get(i).toString())
-                    }
-                }
-            })
-        }
-    }
-
-    fun createMyDataClass(name: String, number: Int): MyDataClass{
-        return MyDataClass(name, number)
-    }
-
-    data class MyDataClass(var name: String, val number: Int){
-
     }
 
 }
