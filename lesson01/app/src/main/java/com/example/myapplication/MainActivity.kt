3091cf0280f4fe2843f74d1ab22436eac1aba987
Sergey <KozlovtsevSV@gmail.com>
2021-05-06 04:00:32 +0300

    HW#1add

--- lesson01/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ lesson01/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,11 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import androidx.core.view.forEach as forEach1
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
